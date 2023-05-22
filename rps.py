b1a5f4189de57fe099b8ffb8dd9c2c05d088d4de
Krizchozz <119966469+StepByStep12345@users.noreply.github.com>
2023-05-22 22:32:32 +0300

    Update rps.py
    Added the option to close/continue the program

--- rps.py
+++ rps.py
@@ -3,35 +3,45 @@ import random
 rock = 'Rock'
 paper = 'Paper'
 scissors = 'Scissors'
-player_move = input("Enter : [r] for Rock, [p] for Paper or [s] for scissors:    ")
-if player_move == "r":
-    player_move = rock
-elif player_move == "p":
-    player_move = paper
-elif player_move == "s":
-    player_move = scissors
-else:
-    raise SystemExit("Invalid Input. Try again...")
 
-print()
-print(f"You chose {player_move}")
-print()
+while True:
+    player_move = input("Enter : [r] for Rock, [p] for Paper or [s] for scissors:   ")
+    if player_move == "r":
+        player_move = rock
+    elif player_move == "p":
+        player_move = paper
+    elif player_move == "s":
+        player_move = scissors
+    else:
+        raise SystemExit("Invalid Input. Try again...")
 
-computer_number = random.randint(1,3)
-computer_move = ''
-if computer_number == 1:
-    computer_move = rock
-elif computer_number == 2:
-    computer_move = paper
-elif computer_number ==3:
-    computer_move = scissors
+    print()
+    print(f"You chose {player_move}")
+    print()
 
-print(f"The computer chose {computer_move}")
-print()
+    computer_number = random.randint(1,3)
+    computer_move = ''
+    if computer_number == 1:
+        computer_move = rock
+    elif computer_number == 2:
+        computer_move = paper
+    elif computer_number ==3:
+        computer_move = scissors
 
-if player_move == rock and computer_move == scissors or player_move == paper and computer_move == rock or player_move == scissors and computer_move == paper:
-    print("You win!!")
-elif player_move == rock and computer_move == paper or player_move == paper and computer_move == scissors or player_move == scissors and computer_move == rock:
-    print("You lost...")
-elif player_move == rock and computer_move == rock or player_move == paper and computer_move == paper or player_move == scissors and computer_move == scissors:
-    print("Its a Draw?")
+    print(f"The computer chose {computer_move}")
+    print()
+
+    if player_move == rock and computer_move == scissors or player_move == paper and computer_move == rock or player_move == scissors and computer_move == paper:
+        print("You win!!")
+    elif player_move == rock and computer_move == paper or player_move == paper and computer_move == scissors or player_move == scissors and computer_move == rock:
+        print("You lost...")
+    elif player_move == rock and computer_move == rock or player_move == paper and computer_move == paper or player_move == scissors and computer_move == scissors:
+        print("Its a Draw?")
+
+    command = input("Do you want to continue? [y] to Continue [n] to close the game:   ")
+    if command == "y":
+        continue
+    elif command == "n":
+        break
+    else:
+        raise SystemExit("Invalid Input. Closing the game")
